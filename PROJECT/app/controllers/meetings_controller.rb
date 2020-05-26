84e871d57feaca6aca6302fb9b4f81053e34640d
Adao <sansonov@gmail.com>
2020-05-26 18:11:17 +0100

    End of day 1

--- PROJECT/app/controllers/meetings_controller.rb
+++ PROJECT/app/controllers/meetings_controller.rb
@@ -1,2 +1,58 @@
 class MeetingsController < ApplicationController
+
+    before_action :set_meeting, only: [:show, :edit, :update, :destroy]
+
+    def show
+    end
+    
+    # def index
+    #     @meetings = Meeting.all
+    # end
+
+    def new
+        @meeting = Meeting.new
+    end
+
+    def create
+        @meeting = Meeting.create(meeting_params)
+        if @meeting.valid?
+          redirect_to @meeting
+        else
+          render :new
+        end
+    end
+
+    # def edit
+    # end
+
+    # def update
+    #     @meeting.update(meeting_params)
+    #     if @meeting.valid?
+    #         redirect_to @meeting
+    #     else
+    #         render "edit"
+    #     end
+    # end
+
+    def destroy
+        #byebug
+        if @meeting.date > Date.today
+            @cult = @meeting.cult
+            @meeting.destroy
+            redirect_to cult_path(@cult)
+        else 
+            flash[:message] = "You cannot delete a meeting that already happened!"   
+            redirect_to @meeting 
+        end
+    end
+
+   
+    private
+    def set_meeting
+        @meeting = Meeting.find(params[:id])
+    end
+
+    def meeting_params
+        params.require(:meeting).permit(:date, :building_id)
+    end
 end
--- PROJECT/app/models/meeting.rb
+++ PROJECT/app/models/meeting.rb
@@ -3,6 +3,15 @@ class Meeting < ApplicationRecord
     has_many :members, through: :attendances
     belongs_to :building
     has_one :location, through: :buildings
-    has_one :cult, through: :buildings
+    has_one :cult, through: :building
 
+    validates :building_id, presence: true
+
+    validate :booked_for_the_future
+
+    def booked_for_the_future
+        if self.date < Date.today
+            self.errors[:base] << "A meeting should be booked for a future date!"
+        end   
+    end
 end
