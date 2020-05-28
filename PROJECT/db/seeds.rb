17f02ad5dfe5cbdf11e207cafdc950b9a217dd7d
Adao <sansonov@gmail.com>
2020-05-28 21:28:10 +0100

    it

--- PROJECT/db/seeds.rb
+++ PROJECT/db/seeds.rb
@@ -1,32 +1,91 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 
 Cult.create(
     [{
         name: "Protoss Fanbois",
         slogan: "You must construct additional Pylons",
-        building_name: "Pylon"
+        building_name: "Pylon",
+        picture: "https://www.pcgamesn.com/wp-content/uploads/legacy/Legacy_of_the_Void.jpg"
     },
     {
         name: "Craft Beer",
         slogan: "Give me that sweet sweet hoppy flavour",
-        building_name: "Pub"
+        building_name: "Pub",
+        picture: "https://www.lacerveteca.com/wp-content/uploads/2018/11/Craft_Beer-2000x1125.jpg"
     },
     {
         name: "Cult of Khorne",
         slogan: "BLOOD FOR THE BLOOD GOD!",
-        building_name: "Death Pit"
+        building_name: "Death Pit",
+        picture: "https://1d4chan.org/images/thumb/1/1a/Cultist_of_tzeentch_by_ilacha-d7hytyz.jpg/450px-Cultist_of_tzeentch_by_ilacha-d7hytyz.jpg"
+    },
+    {
+        name: "Tomb Kings",
+        slogan: "Surrender and serve me in life, or die and slave for me in death.",
+        building_name: "Temple",
+        picture: "https://steamcdn-a.akamaihd.net/steam/apps/617870/ss_a8fc43680cb2f2cf2624a1adb740fe057a7a9461.1920x1080.jpg?t=1571755476"
+    },
+    {
+        name: "Dwarfs",
+        slogan: "No Rest until the Blood of Greenskins flows like Dark Ale upon the Earth!",
+        building_name: "Station",
+        picture: "https://pluspng.com/img-png/7-dwarfs-png-kingdom-hearts-dwarfs-png-1600-667-quotes-posts-pinterest-dwarf-snow-white-and-snow-1600.png"
+    },
+    {
+        name: "Kislev",
+        slogan: "Grimgor iz da best!",
+        building_name: "Church",
+        picture: "https://vignette.wikia.nocookie.net/warhammerfb/images/f/ff/Warhammer_Kislev_Winged_Lancers.png/revision/latest?cb=20180825093027"
+    },
+    {
+        name: "Lizardmen",
+        slogan: "The Maw was born from the fire... The earth shook at its coming... The flames ate many tribes... To the mountains some fled..",
+        building_name: "Park",
+        picture: "https://img00.deviantart.net/7ed9/i/2010/114/e/b/temple_guard_by_daarken.jpg"
+    },
+    {
+        name: "Dark Elves",
+        slogan: "The hungry flame sat in our guts...We ate the mountains to put it out... but the fire was still there...We climbed further still...We ate the Tall Ones' herds... Cavebeast was good meat... We climbed the peaks... Above the skies the giants dwell...",
+        building_name: "Church",
+        picture: "https://lh3.googleusercontent.com/proxy/gqN5tEAur_-QUTuumAIo-uTfOuCaXvFI6p8CCUF3FQGGd5ObiE7paWXz4Y75T8q-iU_RSjomn_jeebSqSn2w6zFAn_iGLhPMdiF715l2lLg2or8rG3Oc3tfVB8hIdzicQPmrmNy7xUTjzfCcbsroew0CbkZUNcJiTSy_XptZfozu38e6SfnsjnneVw"
+    },
+    {
+        name: "Ogre Kingdoms",
+        slogan: "Awake O Dead, for there can be no rest for ye beneath the earth. Let the splintered bones burrow from the grave pall. Let cold fingers grip time-worn blades, and unseeing eyes survey the fields of slaughter. For your time has come once more. And the dead shall walk.",
+        building_name: "Park",
+        picture: "https://cdna.artstation.com/p/assets/images/images/006/096/418/large/michael-tenebrae-ogre-kingdoms-2-smaller.jpg?1496043875"
+    },
+    {
+        name: "Chaos Dwarfs",
+        slogan: "The Ogres were born long ago... Big, strong and fat... The little round ones by our side... We ate and drank till our guts filled...",
+        building_name: "Office",
+        picture: "https://vignette.wikia.nocookie.net/warhammerfb/images/4/46/Hellcancrew_gdesign_01.jpg/revision/latest?cb=20190530033148"
+    },
+    {
+        name: "Mortal Kombat Real Life",
+        slogan: "Flawless victory!",
+        building_name: "Earthrealm",
+        picture: "https://gamespot1.cbsistatic.com/uploads/scale_landscape/1579/15792183/3660965-mk.jpg"
     }
 ]
 )
 
-puts "CUlts Created"
+#----------------------------------------------------------------------------------------------------------------------------------
+#Cults Pictures/URL: 
+
+# Protoss:  https://www.pcgamesn.com/wp-content/uploads/legacy/Legacy_of_the_Void.jpg
+# Craft Beer: https://www.lacerveteca.com/wp-content/uploads/2018/11/Craft_Beer-2000x1125.jpg
+# Cult of Khorne: https://1d4chan.org/images/thumb/1/1a/Cultist_of_tzeentch_by_ilacha-d7hytyz.jpg/450px-Cultist_of_tzeentch_by_ilacha-d7hytyz.jpg
+# Tomb Kings: https://steamcdn-a.akamaihd.net/steam/apps/617870/ss_a8fc43680cb2f2cf2624a1adb740fe057a7a9461.1920x1080.jpg?t=1571755476
+# Dwarfs: https://pluspng.com/img-png/7-dwarfs-png-kingdom-hearts-dwarfs-png-1600-667-quotes-posts-pinterest-dwarf-snow-white-and-snow-1600.png
+# Kislev: https://vignette.wikia.nocookie.net/warhammerfb/images/f/ff/Warhammer_Kislev_Winged_Lancers.png/revision/latest?cb=20180825093027
+# Lizardmen: https://img00.deviantart.net/7ed9/i/2010/114/e/b/temple_guard_by_daarken.jpg
+# Dark Elves: https://lh3.googleusercontent.com/proxy/gqN5tEAur_-QUTuumAIo-uTfOuCaXvFI6p8CCUF3FQGGd5ObiE7paWXz4Y75T8q-iU_RSjomn_jeebSqSn2w6zFAn_iGLhPMdiF715l2lLg2or8rG3Oc3tfVB8hIdzicQPmrmNy7xUTjzfCcbsroew0CbkZUNcJiTSy_XptZfozu38e6SfnsjnneVw
+# Ogre Kingdoms: https://cdna.artstation.com/p/assets/images/images/006/096/418/large/michael-tenebrae-ogre-kingdoms-2-smaller.jpg?1496043875
+# Chaos Dwarfs: https://vignette.wikia.nocookie.net/warhammerfb/images/4/46/Hellcancrew_gdesign_01.jpg/revision/latest?cb=20190530033148
+# MK: https://gamespot1.cbsistatic.com/uploads/scale_landscape/1579/15792183/3660965-mk.jpg
+#--------------------------------------------------------------------------------------------------------------------------
+
+puts "Cults Created"
 
 Location.create([
     {
@@ -72,7 +131,10 @@ Location.create([
     ]
 )
 
-puts "Created Locations"
+puts "Locations Created"
+
+#-----------------------------------------------------------------------------------------------------------
+# # IF YOU USE THIS ONE, THE PICTURES MAY NOT MAKE SENSE, SINCE IT USES FAKER TO CREATE CITIES!!!
 
 # 10.times do 
 #     Location.create(
@@ -80,7 +142,7 @@ puts "Created Locations"
 #          country: Faker::Address.country
 #     )
 # end
-
+#------------------------------------------------------------------------------------------------------------
 
 Member.create(
     [{
@@ -115,30 +177,20 @@ end
 
 puts "Created members"
 
-#cults pictures: 
-# Protoss:  https://www.pcgamesn.com/wp-content/uploads/legacy/Legacy_of_the_Void.jpg
-# Craft Beer: https://www.lacerveteca.com/wp-content/uploads/2018/11/Craft_Beer-2000x1125.jpg
-# Cult of Khorne: https://1d4chan.org/images/thumb/1/1a/Cultist_of_tzeentch_by_ilacha-d7hytyz.jpg/450px-Cultist_of_tzeentch_by_ilacha-d7hytyz.jpg
-# Tomb Kings: https://steamcdn-a.akamaihd.net/steam/apps/617870/ss_a8fc43680cb2f2cf2624a1adb740fe057a7a9461.1920x1080.jpg?t=1571755476
-# Dwarfs: https://pluspng.com/img-png/7-dwarfs-png-kingdom-hearts-dwarfs-png-1600-667-quotes-posts-pinterest-dwarf-snow-white-and-snow-1600.png
-# Kislev: https://vignette.wikia.nocookie.net/warhammerfb/images/f/ff/Warhammer_Kislev_Winged_Lancers.png/revision/latest?cb=20180825093027
-# Lizardmen: https://img00.deviantart.net/7ed9/i/2010/114/e/b/temple_guard_by_daarken.jpg
-# Dark Elves: https://lh3.googleusercontent.com/proxy/gqN5tEAur_-QUTuumAIo-uTfOuCaXvFI6p8CCUF3FQGGd5ObiE7paWXz4Y75T8q-iU_RSjomn_jeebSqSn2w6zFAn_iGLhPMdiF715l2lLg2or8rG3Oc3tfVB8hIdzicQPmrmNy7xUTjzfCcbsroew0CbkZUNcJiTSy_XptZfozu38e6SfnsjnneVw
-# Ogre Kingdoms: https://cdna.artstation.com/p/assets/images/images/006/096/418/large/michael-tenebrae-ogre-kingdoms-2-smaller.jpg?1496043875
-# Chaos Dwarfs: https://vignette.wikia.nocookie.net/warhammerfb/images/4/46/Hellcancrew_gdesign_01.jpg/revision/latest?cb=20190530033148
+#-----------------------------------------------------------------------------------------------------------
+# # IF YOU USE THIS ONE, THE PICTURES MAY NOT MAKE SENSE, SINCE IT USES FAKER TO CREATE NAMES AND SLOGANS!!!
 
+# 10.times do 
+#     Cult.create(
+#         name: Faker::Games::WarhammerFantasy.faction,
+#         slogan: Faker::Games::WarhammerFantasy.quote,
+#         building_name: ["Church", "Office", "Park", "Temple", "Pub", "Station"].sample
+#     )
+# end
 
+# puts "Cults created"
+#----------------------------------------------------------------------------------------------
 
-
-10.times do 
-    Cult.create(
-        name: Faker::Games::WarhammerFantasy.faction,
-        slogan: Faker::Games::WarhammerFantasy.quote,
-        building_name: ["Church", "Office", "Park", "Temple", "Pub", "Station"].sample
-    )
-end
-
-puts "Cults created"
 
 50.times do 
     loc = Location.all.sample
@@ -150,7 +202,7 @@ puts "Cults created"
     )
 end
 
-puts "Created buildings"
+puts "Buildings Created"
 
 20.times do 
     member = Member.all.sample
@@ -162,7 +214,7 @@ puts "Created buildings"
 
 end
 
-puts "created Oaths"
+puts "Oaths Created"
 
 50.times do 
     building = Building.all.sample
@@ -189,6 +241,6 @@ puts "Meetings created"
     )
 end
 
-puts "Created Attendances"
+puts "Attendances Created"
 
-puts "Seed Complete"
+puts "Seed Complete!!!"
